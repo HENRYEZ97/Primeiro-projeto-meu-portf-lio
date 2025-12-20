ec41d1e00d21a46c75de49b068391acc1ec699e0
HENRYEZ97 <ricksillva97@gmail.com>
2025-12-19 21:22:53 -0400

    ✅ Componente MeusTemas.tsx excluído.

--- portfolio-octavio/app/page.tsx
+++ portfolio-octavio/app/page.tsx
@@ -1,5 +1,4 @@
 import SectionTop from "./componentes/SectionTop";
-import SectionTemas from "./componentes/SectionTemas";
 import SectionSobre from "./componentes/SectionSobre";
 import SectionPortfolio from "./componentes/SectionPortfolio";
 import SectionForm from "./componentes/SectionForm";
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <main className="bg-black min-h-screen">
       <SectionTop />
-      <SectionTemas />
       <SectionSobre />
       <SectionPortfolio />
       <SectionForm />
