1cfc4bf7cad621f7cf7efa20ec218d42123846b8
HENRYEZ97 <ricksillva97@gmail.com>
2025-12-22 19:54:39 -0400

    ✨2 componentes reutilizáveis criados.

--- portfolio-octavio/app/data/projetos.ts
+++ portfolio-octavio/app/data/projetos.ts
@@ -6,9 +6,32 @@ export const projetos = [
         descricao: "O EZ Chat é um sistema de comunicação desenvolvido por mim para uma empresa de pequeno porte para facilitar a troca de mensagens em tempo real entre departamentos.",
         tecnologias: ["Next.js", "TypeScript", "Node.js", "WebSocket", "TailwindCSS"],
         imagens: [
-            "/imagens/1.png",
-            "",
-            "",
+            "/imagens/1.png"
+        ],
+        github: ""
+    },
+
+    {
+        slug: "Hydrosense",
+        titulo: "HYDROSENSE",
+        resumo: "Sistema de Monitoramento de áreas de risco",
+        descricao: "O Hydrosense é um sistema de alerta de enchentes e monitoramento em tempo real de áreas de riscos na cidade de Manaus",
+        tecnologias: ["Next.js", "Typescript", "Node.js", "Websocket", "TailwindCSS", "ESP32", "Sensor Ultrassônico"],
+        imagens: [
+            "/imagens/2.png"
+        ],
+        github: ""
+    },
+
+
+    {
+        slug: "Sistemas de pedidos",
+        titulo: "Sistema de pedidos de lanches",
+        resumo: "Sistemas para fazer pedidos de lanches com facilidade e rapidez",
+        descricao: "Sistema para fazer pedido de lanches com mais facilidade com encaminhamento direto para o whatsapp do estabelecimento.",
+        tecnologias: ["React", "Vite", "JavaScript", "HTML", "CSS"],
+        imagens: [
+            "/imagens/3.png"
         ],
         github: ""
     }
