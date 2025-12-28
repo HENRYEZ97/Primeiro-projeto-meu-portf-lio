b975ca7412a72265c3965a75f95dc89930eb587b
HENRYEZ97 <ricksillva97@gmail.com>
2025-12-28 13:25:34 -0400

    💄Testando efeitos de fundo em seções no site.

--- portfolio-octavio/app/componentes/SectionTop.tsx
+++ portfolio-octavio/app/componentes/SectionTop.tsx
@@ -1,35 +1,31 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export default function SectionTop() {
   return (
-    <section id="inicio" className="relative py-32 px-6 overflow-hidden bg-gradient-to-t from-black to-neutral-900">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-20">
+    <section
+      id="inicio"
+      className="relative py-32 px-6 overflow-hidden bg-gradient-to-t from-black to-neutral-900">
+      <div className="absolute inset-0 pointer-events-none">
+        <div className="absolute -top-40 -left-40 w-[600px] h-[600px] bg-blue-600/30 rounded-full blur-[160px] animate-bg1"/>
+        <div className="absolute top-1/3 -right-40 w-[500px] h-[500px] bg-purple-600/30 rounded-full blur-[160px] animate-bg2"/>
+      <div className="absolute bottom-0 left-1/3 w-[600px] h-[600px] bg-cyan-500/20 rounded-full blur-[160px] animate-bg3"/>
+    </div>
+      <div className="relative z-10 max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-20">
         <div>
           <h1 className="text-5xl md:text-6xl font-bold leading-tight">
             Laboratório de Códigos<span className="text-blue-700">.</span>
-          </h1>
-
-          <p className="mt-6 text-lg text-gray-300 max-w-xl">
-            Transformando ideias em soluções reais através da programação,
-            explorando tecnologia moderna e boas práticas.
-          </p>
-
+             </h1>
+            <p className="mt-6 text-lg text-gray-300 max-w-xl">
+          Transformando ideias em soluções reais através da programação,
+        explorando tecnologia moderna e boas práticas.
+         </p>
           <Link href="#portfolio">
-            <button className="cursor-pointer mt-10 px-8 py-3 bg-blue-700 hover:bg-blue-600 rounded-full shadow-lg transition">
-              Ver projetos
-            </button>
-          </Link>
-        </div>
-
-        <Image
-          src="/imagens/ez.png.jpg"
-          alt="Imagem topo"
-          width={480}
-          height={480}
-          className="animate-float"
-        />
-      </div>
+            <button className="mt-10 px-8 py-3 bg-blue-700 hover:bg-blue-600 rounded-full shadow-lg transition cursor-pointer">
+             Ver projetos
+             </button>
+              </Link>
+             </div>
+           </div>
     </section>
   );
 }
